0b6c55d8964bb2470d40d3a6112f5b6602c889de
Dustin Ewers <dustin.ewers@gmail.com>
2016-06-28 09:17:22 -0500

    Added an Async Demo

---
+++ perfDemo/perfDemo/Controllers/TestController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using StackExchange.Profiling;
+
+namespace perfDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        private const int NumberOfWorkCycles = 5;
+        private const int SleepTime = 250;
+        
+        public ActionResult TestAsync()
+        {
+
+            var profiler = MiniProfiler.Current; // it's ok if this is null
+            using (profiler.Step("Making Calls"))
+            {
+                using (profiler.Step("Async Work"))
+                {
+                    AsyncWork(SleepTime);
+                }
+
+                using (profiler.Step("Sync Work"))
+                {
+                    SyncWork(SleepTime);
+                }
+            }
+
+            return View();
+        }
+
+        public void AsyncWork(int seconds)
+        {
+            var work = new List<Task>();
+
+            for (var i = 0; i < NumberOfWorkCycles; i++)
+            {
+                var task = Task.Run(() => Thread.Sleep(seconds));
+                work.Add(task);
+            }
+
+            Task.WaitAll(work.ToArray());
+        }
+
+        public void SyncWork(int seconds)
+        {
+            for (var i = 0; i < NumberOfWorkCycles; i++)
+            {
+                Thread.Sleep(seconds);
+            }
+        }
+        
+        #region "Serializer Test"
+        // GET: Test
+        //public ActionResult TestSerializer()
+        //{
+            
+        //    return View();
+        //}
+        
+        //public class Foo
+        //{
+        //    public string Name { get; set; }
+        //    public string Quest { get; set; }
+        //    public string FavoriteColor { get; set; }
+
+        //    public List<Bar> Bars { get; set; }
+        //}
+
+        //public class Bar
+        //{
+        //    public string Description { get; set; }
+        //    public string Color { get; set; }
+        //    public Foo Foo { get; set; }
+        //}
+        
+        //public class ComplexEntity
+        //{
+        //    public List<Foo> Foos { get; set; }
+        //    public List<Bar> Bars { get; set; }
+        //}
+        #endregion
+
+        public enum AsyncValue
+        {
+            Async, 
+            Sync
+        }
+    }
+}
--- perfDemo/perfDemo/Global.asax.cs
+++ perfDemo/perfDemo/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using StackExchange.Profiling;
 using StackExchange.Profiling.EntityFramework6;
+using StackExchange.Profiling.Mvc;
 
 namespace perfDemo
 {
@@ -10,6 +11,10 @@ namespace perfDemo
     {
         protected void Application_Start()
         {
+            // We Don't need Web Forms in our MVC App.
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
