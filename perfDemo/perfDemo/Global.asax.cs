7c04a299e12ca383a7c596ad30c5d4f92eda1c74
Dustin Ewers <dustin.ewers@gmail.com>
2016-06-30 07:54:56 -0500

    Added slides and bundling demo
    also switched to the less version of bootstrap

--- perfDemo/perfDemo/Global.asax.cs
+++ perfDemo/perfDemo/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using StackExchange.Profiling;
 using StackExchange.Profiling.EntityFramework6;
-using StackExchange.Profiling.Mvc;
 
 namespace perfDemo
 {
